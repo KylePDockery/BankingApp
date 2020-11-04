26e3f56f8072fdc753fe58f8fcab07171b93141e
Kyle P. Dockery <KylePDockery@gmail.com>
2020-11-04 08:54:01 -0600

    working banking app

---
+++ src/com/banking_app/KylesBank.java
@@ -0,0 +1,13 @@
+package com.banking_app;
+
+public class KylesBank {
+
+    public static void main(String[] args) {
+
+        Account kyle = new Account("Kyle Dockery", "1234");
+
+
+Account kat = new Account("Gena Rois", "1523");
+kat.showMenu();
+    }
+}
