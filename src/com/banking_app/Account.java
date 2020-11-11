5d4b2a147c865af9d72b80297e61cbbf50993c16
Kyle P Dockery <KylePDockery@gmail.com>
2020-11-11 08:32:09 -0600

    reformating, currency added

--- src/com/banking_app/Account.java
+++ src/com/banking_app/Account.java
@@ -1,120 +1,116 @@
 package com.banking_app;
+
 import java.util.Scanner;
+import java.text.NumberFormat;
 
 public class Account {
-    int balance;
-    int previousTransaction;
+    double balance;
+    double previousTransaction;
     String customerName;
     String customerID;
 
-    Account(String cname, String cid){
+    Account(String cname, String cid) {
         customerName = cname;
         customerID = cid;
     }
 
-    void deposit(int amount){
-        if (amount > 0){
+    private void deposit(double amount) {
+        if (amount > 0) {
             balance += amount;
             previousTransaction = amount;
         }
     }
 
-    void withdraw(int amount){
-        if(amount > 0){
+    private void withdraw(double amount) {
+        if (amount > 0) {
             balance -= amount;
             previousTransaction = -amount;
         }
     }
 
-    void getPreviousTransaction(){
-        if(previousTransaction > 0){
+    private void getPreviousTransaction() {
+        if (previousTransaction > 0) {
             System.out.println("Deposited " + previousTransaction);
-        }
-        else if (previousTransaction < 0){
-            System.out.println("Withdrawn: " + Math.abs(previousTransaction) );
-        }
-        else{
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
             System.out.println("No transaction occurred");
         }
     }
 
 
-void calculateInterest(int years){
-    double interestRate = .0185;
-    double newBalance = (balance * interestRate * years) + balance;
-    System.out.println("The current interest rate is " + (100 + interestRate));
-    System.out.println("After " + years + " years, the balance will be: " + newBalance);
-}
-void menuText(){
-    System.out.println("What would you like to do?");
-    System.out.println();
-    System.out.println("A. Check your balance");
-    System.out.println("B. Make a deposit");
-    System.out.println("C. Make a withdrawal");
-    System.out.println("D. View previous transaction");
-    System.out.println("E. Calculate interest");
-    System.out.println("F. Exit");
-}
+    private void calculateInterest(int years) {
+        NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
+        double interestRate = .0185;
+        double newBalance = (balance * interestRate * years) + balance;
+        System.out.println("The current interest rate is " + (100 * interestRate) + "%");
+        System.out.println("After " + years + " years, the balance will be: " + defaultFormat.format(newBalance));
+    }
 
-void showMenu() {
-    char option;
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Welcome, " + customerName + "!");
-    System.out.println("You ID is: " + customerID);
-    System.out.println();
-    menuText();
+    private void menuText() {
+        System.out.println("What would you like to do?\n");
+        System.out.println("""
+                A. Check your balance
+                B. Make a deposit
+                C. Make a withdrawal
+                D. View previous transaction
+                E. Calculate interest
+                F. Exit""");
+    }
 
-    do {
-        System.out.println();
-        System.out.println("Enter an option: ");
-        char option1 = scanner.next().charAt(0);
-        option = Character.toUpperCase(option1);
-        System.out.println();
+    void showMenu() {
+        char option;
+        Scanner scanner = new Scanner(System.in);
+        NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
+        System.out.println("Welcome, " + customerName + "!");
+        System.out.println("You ID is: " + customerID + "\n");
+        menuText();
 
-        switch (option) {
-            case 'A' -> {
-                System.out.println("==============");
-                System.out.println("Balance = $" + balance);
-                System.out.println("==============");
-                System.out.println();
-                menuText();
+        do {
+            System.out.println("\n Enter an option: ");
+            char option1 = scanner.next().charAt(0);
+            option = Character.toUpperCase(option1);
+            System.out.println();
+
+            switch (option) {
+                case 'A' -> {
+                    System.out.println("==============\n Balance = " + defaultFormat.format(balance) +
+                            "\n============== \n");
+                    menuText();
+                }
+                case 'B' -> {
+                    System.out.println("Enter an amount to deposit \n");
+                    double amount = scanner.nextDouble();
+                    deposit(amount);
+                    System.out.println("You have deposited " + defaultFormat.format(amount) +
+                            "\nYour new balance is " + defaultFormat.format(balance));
+                    menuText();
+                }
+                case 'C' -> {
+                    System.out.println("enter an amount to withdraw: \n");
+                    double amount2 = scanner.nextDouble();
+                    withdraw(amount2);
+                    System.out.println("You have withdrawn " + defaultFormat.format(amount2) +
+                            "\nYour new balance is " + defaultFormat.format(balance));
+                    menuText();
+                }
+                case 'D' -> {
+                    System.out.println("==================");
+                    getPreviousTransaction();
+                    System.out.println("==================\n");
+                    menuText();
+                }
+                case 'E' -> {
+                    System.out.println("Enter how many years of accrued interest: ");
+                    int years = scanner.nextInt();
+                    calculateInterest(years);
+                    System.out.println("Your current balance is " + defaultFormat.format(balance));
+                    menuText();
+                }
+                case 'F' -> System.out.println("===================");
+                default -> System.out.println("Error: invalid option. Please enter another option: A, B, C, D, E, or F");
             }
-            case 'B' -> {
-                System.out.println("Enter an amount to deposit");
-                int amount = scanner.nextInt();
-                deposit(amount);
-                System.out.println();
-                System.out.println("You have deposited " + amount);
-                System.out.println("Your new balance is " + balance);
-                menuText();
-            }
-            case 'C' -> {
-                System.out.println("enter an amount to withdraw: ");
-                int amount2 = scanner.nextInt();
-                withdraw(amount2);
-                System.out.println();
-                System.out.println("You have withdrawn " + amount2);
-                System.out.println("Your new balance is " + balance);
-                menuText();
-            }
-            case 'D' -> {
-                System.out.println("==================");
-                getPreviousTransaction();
-                System.out.println("==================");
-                System.out.println();
-                menuText();
-            }
-            case 'E' -> {
-                System.out.println("Enter how many years of accrued interest: ");
-                int years = scanner.nextInt();
-                calculateInterest(years);
-                System.out.println("Your current balance is " + balance);
-                menuText();
-            }
-            case 'F' -> System.out.println("===================");
-            default -> System.out.println("Error: invalid option. Please enter another option: A, B, C, D, E, or F");
-        }
-    } while (option != 'F');
-    System.out.println("Thank you for using my bank app");
-}
+        } while (option != 'F');
+        System.out.println("Thank you for using Kyle's First Bank of Java!");
+    }
 }
--- src/com/banking_app/KylesBank.java
+++ src/com/banking_app/KylesBank.java
@@ -3,11 +3,8 @@ package com.banking_app;
 public class KylesBank {
 
     public static void main(String[] args) {
-
-        Account kyle = new Account("Kyle Dockery", "1234");
-
-
-Account kat = new Account("Gena Rois", "1523");
-kat.showMenu();
+        System.out.println("Welcome to Kyle's First Bank of Java. \n");
+        Account kat = new Account("Gena Rois", "1523");
+        kat.showMenu();
     }
 }
